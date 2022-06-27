4f9e1b9bbd7f9e4a09c89ab035ffa3f6bf2a8b09
Pete Goodwin <imekon@users.noreply.github.com>
2022-06-27 07:57:41 +0100

    added engine wrapper class

---
+++ Engine/StkEngine.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Engine
+{
+    public static class StkEngine
+    {
+        [DllImport("StkEngine2.dll", CallingConvention = CallingConvention.StdCall)]
+        public extern static void Initialise();
+        [DllImport("StkEngine2.dll", CallingConvention = CallingConvention.StdCall)]
+        public extern static void Shutdown();
+        [DllImport("StkEngine2.dll", CallingConvention = CallingConvention.StdCall)]
+        public extern static void Start();
+        [DllImport("StkEngine2.dll", CallingConvention = CallingConvention.StdCall)]
+        public extern static void Stop();
+    }
+}
